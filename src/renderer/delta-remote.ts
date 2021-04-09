5b318b4ffba9ff8733f595c42a2fec2a19607536
jikstra <jikstra@disroot.org>
2021-04-09 03:25:08 +0200

    Fix deleting message

--- src/renderer/delta-remote.ts
+++ src/renderer/delta-remote.ts
@@ -274,7 +274,7 @@ class DeltaRemote {
     fnName: 'messageList.saveMessageHTML2Disk',
     messageId: number
   ): Promise<string>
-  call(
+  call( 
     fnName: 'messageList.getFirstUnreadMessageId',
     chatId: number
   ): Promise<number>
